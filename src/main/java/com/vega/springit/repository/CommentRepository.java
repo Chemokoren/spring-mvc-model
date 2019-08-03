7146bd07a170b4a29ee882e2fbd4f96f7127ae17
Kibet Masai <humphreykibet@gmail.com>
2019-08-03 06:00:18 +0300

    sorted repository hkm

---
+++ src/main/java/com/vega/springit/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.vega.springit.repository;
+
+
+import com.vega.springit.domain.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository  extends JpaRepository<Comments, Long> {
+}
