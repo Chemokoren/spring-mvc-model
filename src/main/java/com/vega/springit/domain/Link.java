46973e50778e213217dfcf8520ed94fef3017a76
Kibet Masai <humphreykibet@gmail.com>
2019-08-10 05:10:19 +0300

    sorted view layer hkm

--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -1,26 +1,55 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Getter@Setter
 @RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Link extends Auditable {
+
+//    public Link(String k, String v) {
+//        super();
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
+
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
+    @URL(message = "Please enter a valid URL.")
     private String url;
 
     //    comments
@@ -28,5 +57,21 @@ public class Link extends Auditable {
     private List<Comments> comments = new ArrayList<>();
 
     public void addComment(Comments comment) {
+        comments.add(comment);
+    }
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
